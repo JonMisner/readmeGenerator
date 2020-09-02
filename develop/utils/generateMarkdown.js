2d9d39a5d0aa2e20ece64422ea0a81ec66ac6d6b
JonM <jonmisner86@gmail.com>
2020-09-01 23:28:13 -0500

    trying to fix license

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 function generateMarkdown(data) {
   return `
   # ${data.title}  
-  https://img.shields.io/badge/license-${data.license}-blue
+  ![github license](https://img.shields.io/badge/license-${data.license}-blue)
 ***
  
   ## Description
